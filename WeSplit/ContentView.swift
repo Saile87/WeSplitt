2f1fe7478851d8f5679bd0e68d5a8e4314b667e2
Elias Breitenbach <elias.b@me.com>
2023-03-15 20:57:57 +0100

    Pushed project to github

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,88 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Elias Breitenbach on 20.01.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var betragPruefen = 0.0
+    @State private var anzahlPersonen = 2
+    @State private var trinkgeldProzentual = 0
+    @FocusState private var amountIsFocused: Bool
+    
+    
+    var totalPerPerson: Double {
+        let personenZaehler = Double(anzahlPersonen + 2)
+        let prozentAuswahl = Double(trinkgeldProzentual)
+        
+        let prozentWert = betragPruefen / 100 * prozentAuswahl
+        let gesamtSumme = betragPruefen + prozentWert
+        let betragProPerson = gesamtSumme / personenZaehler
+        
+        return betragProPerson
+    }
+    
+    var rechnungsSumme: Double {
+        let prozentAuswahl2 = Double(trinkgeldProzentual)
+        let prozentWert2 = betragPruefen / 100 * prozentAuswahl2
+        let scheck = betragPruefen + prozentWert2
+        return scheck
+        
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Betrag", value: $betragPruefen, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Anzahl Personen", selection: $anzahlPersonen) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section {
+                    Picker("Prozentual", selection: $trinkgeldProzentual) {
+                        ForEach(0..<101, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                } header: {
+                    Text("Höhe des Trinkgeldes")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                } header: {
+                    Text("Betrag pro Person")
+                }
+                Section {
+                    Text(rechnungsSumme, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                } header: {
+                    Text("Rechnungsbetrag Gesamt")
+                }
+            }
+            .navigationTitle("We Splitt")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Elias Breitenbach on 20.01.23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
